094d8e179fc51150f4590ff0aaeb5c44d6d084d3
Ducvnn <duc.vnnd@gmail.com>
2024-06-25 15:06:53 +0700

    video 30

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -1,9 +1,25 @@
 // arrow function === bien name = () => {}
-// component === html + css + js (Dat ten component quy tac viet hoa chu cai dau)
+// component === html + css + js ( Dat ten component quy tac viet hoa chu cai dau )
+
+// JSX: chi dung 1 parent
+// fragment === <> </> ( khong render nhieu html )
+
+
+
+import './style.css';
 
 const MyComponent = () => {
   return (
-    <div> eric & hoi dan it update </div>
+    <>
+      <div> eric & hoi dan it update </div>
+      <div className="child"
+          style={
+            { boderRadius: "10px" }
+          }
+      >child</div>
+      
+    </>
+    
   );
 }
 export default MyComponent;
