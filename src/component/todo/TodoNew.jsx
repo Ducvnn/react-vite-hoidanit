22906c2bc26ef4690b0508244fea647e19524cb7
Ducvnn <duc.vnnd@gmail.com>
2024-06-27 16:01:40 +0700

    video 36

--- src/component/todo/TodoNew.jsx
+++ src/component/todo/TodoNew.jsx
@@ -3,13 +3,46 @@ const TodoNew = (props) => {
   console.log(`>>> check props`, props)
   const { addNewTodo } = props;
 
-  addNewTodo ()
+  // addNewTodo ()
+  
+  const handleClick = () => {
+    alert(`click me`)
+  }
+
+  // const handleOnChange = (event) => {
+  //   console.log(`>>>>handleOnChange`, event.target.value)
+  // }
+
+  // return (
+  //   <div className='todo-new'>
+  //       <input type="text" 
+  //       onChange={handleOnChange}
+  //       />
+  //       <button
+  //         style={{ cursor: "pointer" }}
+  //         // onclick={() => handleClick()}
+  //         onClick={handleClick}
+  //       >Add</button>
+  //     </div>
+  // ) vd 1
+
+  const handleOnChange = (name) => {
+    console.log(`>>>>handleOnChange`, name)
+  }
+
   return (
     <div className='todo-new'>
-        <input type="text" />
-        <button>Add</button>
+        <input type="text" 
+        onChange={ (event) => handleOnChange(event.target.value)}
+        />
+        <button
+          style={{ cursor: "pointer" }}
+          // onclick={() => handleClick()}
+          onClick={handleClick}
+        >Add</button>
       </div>
   )
+
 }
 
 export default TodoNew;
