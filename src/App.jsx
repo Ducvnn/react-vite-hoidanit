19b56995c765a9b008477731390c6e4c111a51af
Ducvnn <duc.vnnd@gmail.com>
2024-06-27 14:54:50 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import './component/todo/todo.css' ;
 import TodoNew from './component/todo/TodoNew' ;
 import TodoData from './component/todo/TodoData';
 import reactLogo from './assets/react.svg';
-const App = () => {
 
+
+const App = () => {
+  const hoidanit = "eric";
+  const age = 25;
+  const data = {
+    address: "hanoi",
+    country: "vietnam"
+  }
+  
   return (
     <div className="todo-container">
       <div className="todo-title"> TODO LIST </div>
-      <TodoNew/>
-      <TodoData/>
+      <TodoNew />
+      <TodoData 
+        name={hoidanit}
+        age={age}
+        data={data}
+      />
       <div className='todo-image'>
         <img src={reactLogo} className='logo' />
       </div>
--- src/component/todo/TodoData.jsx
+++ src/component/todo/TodoData.jsx
@@ -1,8 +1,23 @@
 
-const TodoData = () => {
+//{key:value}
+//props la mot bien object {}
+// vd {
+//   name: "eric",
+//   age: 25,
+//   data: {}
+// }
+
+const TodoData = (props) => {
   
+// const name = props.name;
+// const age = props.age;
+// const data = props.data;
+
+  const {name, age, data} = props; // cu phap destructuring object
+  console.log(`>>>check props`, props)
   return (
     <div className='todo-data'>
+      <dive> My name is {props.name} </dive>
       <div> Learning React </div>
       <div> Watching Youtoube </div>
       </div>
