5e859dfc8d5b44e44065c408716a71dd79d195d7
Ducvnn <duc.vnnd@gmail.com>
2024-07-02 17:10:33 +0700

    video 42

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,12 @@ const App = () => {
     return Math.floor(Math.random() * (max - min + 1) + min);
     }
   
+  const deleteTodo = (id) => {
+    const newTodo = todoList.filter(item => item.id !== id)
+    console.log(`>>> newTodo:`, newTodo)
+    setTodoList(newTodo)
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title"> TODO LIST </div>
@@ -38,6 +44,7 @@ const App = () => {
       {todoList.length > 0 ?
         <TodoData 
         todoList={todoList}
+        deleteTodo={deleteTodo}
         />
       :
         <div className='todo-image'>
--- src/component/todo/TodoData.jsx
+++ src/component/todo/TodoData.jsx
@@ -1,13 +1,21 @@
 const TodoData = (props) => {
-  const {todoList } = props;
+  
+  const {todoList, deleteTodo } = props;
+
+  const handleClick = (id) => {
+    deleteTodo(id)
+  }
+
   return (
     <div className='todo-data'>
       {todoList.map((item, index) => {
         console.log(`>>> check map:`, item, index)
         return (
           <div className="todo-item" key={item.id}>
-            <div> {item.name}</div>
-            <button>Delete</button>
+            <div>{item.name}</div>
+            <button 
+              onClick={() =>handleClick(item.id)}
+              style={{cursor: "pointer"}}>Delete</button>
           </div>)
       })}
       
